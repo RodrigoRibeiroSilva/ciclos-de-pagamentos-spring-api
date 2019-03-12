7a3c507120fb1de23d55e1c08555014168680ec8
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-11 22:04:44 -0300

    Projeto criado

---
+++ src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigor.ciclosdepagamentosspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiclosDePagamentosSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiclosDePagamentosSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodrigor.ciclosdepagamentosspringapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CiclosDePagamentosSpringApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

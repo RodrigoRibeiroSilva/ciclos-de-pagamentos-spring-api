da20552de19d19a81fadfeee505eab0673e132da
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-12 20:55:18 -0300

    Implementada ação para popular o banco h2 sempre que o app iniciar

--- src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
+++ src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
@@ -1,13 +1,32 @@
 package com.rodrigor.ciclosdepagamentosspringapi;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CiclosDePagamentosSpringApiApplication {
+import com.rodrigor.ciclosdepagamentosspringapi.domain.Categoria;
+import com.rodrigor.ciclosdepagamentosspringapi.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CiclosDePagamentosSpringApiApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CiclosDePagamentosSpringApiApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cagetoria1 = new Categoria(null, "Informática");
+		Categoria cagetoria2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cagetoria1, cagetoria2));
+		
 	}
 
 }

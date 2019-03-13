c8e09f463de0445941bc12e32909d061ff2cc849
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-13 20:50:48 -0300

    Implementado os modelos Estado e Cidade

--- src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
+++ src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rodrigor.ciclosdepagamentosspringapi.domain.Categoria;
+import com.rodrigor.ciclosdepagamentosspringapi.domain.Cidade;
+import com.rodrigor.ciclosdepagamentosspringapi.domain.Estado;
 import com.rodrigor.ciclosdepagamentosspringapi.domain.Produto;
 import com.rodrigor.ciclosdepagamentosspringapi.repositories.CategoriaRepository;
+import com.rodrigor.ciclosdepagamentosspringapi.repositories.CidadeRepository;
+import com.rodrigor.ciclosdepagamentosspringapi.repositories.EstadoRepository;
 import com.rodrigor.ciclosdepagamentosspringapi.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CiclosDePagamentosSpringApiApplication implements CommandLineRunner
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CiclosDePagamentosSpringApiApplication.class, args);
@@ -43,6 +51,20 @@ public class CiclosDePagamentosSpringApiApplication implements CommandLineRunner
 		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
 		produtoRepository.saveAll(Arrays.asList(produto1,produto2,produto3));
+		
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+		
+		Cidade cidade1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade cidade2 = new Cidade(null, "Uberlândia", estado2);
+		Cidade cidade3 = new Cidade(null, "Campinas", estado2);
+		
+		estado1.getCidades().addAll(Arrays.asList(cidade1));
+		estado2.getCidades().addAll(Arrays.asList(cidade2, cidade3));
+		
+		
+		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
+		cidadeRepository.saveAll(Arrays.asList(cidade1, cidade2, cidade3));
 		
 	}
 

edbecc97a1e4cae63b09f94e3477cf09a97e9ac3
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-12 21:23:30 -0300

    Implementada a classe Produto e mapeado o relacionamento muitos para muitos com a Categoria

--- src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
+++ src/main/java/com/rodrigor/ciclosdepagamentosspringapi/CiclosDePagamentosSpringApiApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rodrigor.ciclosdepagamentosspringapi.domain.Categoria;
+import com.rodrigor.ciclosdepagamentosspringapi.domain.Produto;
 import com.rodrigor.ciclosdepagamentosspringapi.repositories.CategoriaRepository;
+import com.rodrigor.ciclosdepagamentosspringapi.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CiclosDePagamentosSpringApiApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CiclosDePagamentosSpringApiApplication.class, args);
@@ -22,10 +26,23 @@ public class CiclosDePagamentosSpringApiApplication implements CommandLineRunner
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria cagetoria1 = new Categoria(null, "Informática");
-		Categoria cagetoria2 = new Categoria(null, "Escritório");
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
 		
-		categoriaRepository.saveAll(Arrays.asList(cagetoria1, cagetoria2));
+		
+		Produto produto1 = new Produto(null, "Computador", 5000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+		categoria2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
+		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		produtoRepository.saveAll(Arrays.asList(produto1,produto2,produto3));
 		
 	}
 

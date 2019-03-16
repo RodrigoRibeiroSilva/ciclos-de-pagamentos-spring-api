eeaaa4ade557ee24b21535c0aca7c4b38174004c
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-16 14:49:36 -0300

    Listando todas as categorias usando DTO

--- src/main/java/com/rodrigor/ciclosdepagamentosspringapi/resources/CategoriaResource.java
+++ src/main/java/com/rodrigor/ciclosdepagamentosspringapi/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.rodrigor.ciclosdepagamentosspringapi.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigor.ciclosdepagamentosspringapi.domain.Categoria;
+import com.rodrigor.ciclosdepagamentosspringapi.dto.CategoriaDTO;
 import com.rodrigor.ciclosdepagamentosspringapi.services.CategoriaService;
 
 @RestController
@@ -20,6 +23,14 @@ public class CategoriaResource {
 	
 	@Autowired
 	private CategoriaService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> listaCategoria  = service.findAll();
+		List<CategoriaDTO> listaCategoriaDTO = listaCategoria.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listaCategoriaDTO);
+	}
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {

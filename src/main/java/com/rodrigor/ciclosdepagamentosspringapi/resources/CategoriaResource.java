ea085d1c17d869f10e7749aac0f2b37b68e3a41d
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-16 13:55:33 -0300

    Implementado o método POST para a Categoria

--- src/main/java/com/rodrigor/ciclosdepagamentosspringapi/resources/CategoriaResource.java
+++ src/main/java/com/rodrigor/ciclosdepagamentosspringapi/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.rodrigor.ciclosdepagamentosspringapi.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigor.ciclosdepagamentosspringapi.domain.Categoria;
 import com.rodrigor.ciclosdepagamentosspringapi.services.CategoriaService;
@@ -19,8 +23,17 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> findById(@PathVariable Integer id) {
-		
 		Categoria obj = service.findById(id);
+		
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }

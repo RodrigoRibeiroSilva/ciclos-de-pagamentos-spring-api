f8bf55e5984b9ccc24e98a2df1e5c00ea003b76c
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-03-18 21:55:28 -0300

    Implementadas as rotas do modelo Cliente

--- src/main/java/com/rodrigor/ecommerce/resources/ClienteResource.java
+++ src/main/java/com/rodrigor/ecommerce/resources/ClienteResource.java
@@ -1,13 +1,24 @@
 package com.rodrigor.ecommerce.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigor.ecommerce.domain.Cliente;
+import com.rodrigor.ecommerce.dto.ClienteDTO;
 import com.rodrigor.ecommerce.services.ClienteService;
 
 @RestController
@@ -17,10 +28,60 @@ public class ClienteResource {
 	@Autowired
 	private ClienteService service;
 	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		
+		List<Cliente> listaCliente  = service.findAll();
+		List<ClienteDTO> listaClienteDTO = listaCliente.stream().map(categoria -> new ClienteDTO(categoria)).collect(Collectors.toList());
+		
+		return ResponseEntity.ok().body(listaClienteDTO);
+	}
+	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
 		
 		Cliente obj = service.findById(id);
 		return ResponseEntity.ok(obj);
+	}
+	
+	@RequestMapping(value="/page",method=RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			
+			@RequestParam(value="page", defaultValue="0") Integer page,
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction ) {
+		
+		Page<Cliente> listaCliente  = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listaClienteDTO = listaCliente.map(categoria -> new ClienteDTO(categoria));
+		
+		return ResponseEntity.ok().body(listaClienteDTO);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteDTO categoriaDto){
+		
+		Cliente categoria = service.fromDTO(categoriaDto);
+		categoria = service.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO categoriaDTO, @PathVariable Integer id){
+		
+		Cliente categoria = service.fromDTO(categoriaDTO);
+		categoria.setId(id);
+		categoria = service.update(categoria);
+		
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Void> remove(@PathVariable Integer id) {
+		
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/rodrigor/ecommerce/services/ClienteService.java
+++ src/main/java/com/rodrigor/ecommerce/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.rodrigor.ecommerce.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.rodrigor.ecommerce.domain.Cliente;
+import com.rodrigor.ecommerce.dto.ClienteDTO;
 import com.rodrigor.ecommerce.repositories.ClienteRepository;
+import com.rodrigor.ecommerce.services.exceptions.DataIntegrityException;
 import com.rodrigor.ecommerce.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,11 +22,50 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
 	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
-
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente cliente = findById(obj.getId());
+		updateDate(cliente, obj);
+		return repo.save(cliente);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir pois existem entidades relacionadas.");
+		}
+		
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDto) {
+		return new Cliente(clienteDto.getId(), clienteDto.getNome(), clienteDto.getEmail(), null, null);
+	}
+	
+	private void updateDate(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
